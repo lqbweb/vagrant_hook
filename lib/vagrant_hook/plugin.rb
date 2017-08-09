47751ad0104fd35ae49ae92d2726e0b6ac7f5afe
Ruben <ruben@astraia.com>
2017-08-09 16:59:24 +0200

    adding on_prepare_clone

--- lib/vagrant_hook/plugin.rb
+++ lib/vagrant_hook/plugin.rb
@@ -2,13 +2,25 @@ module VagrantPlugins
   module VagrantHook
 	class HooksConfig < Vagrant.plugin(2, :config)
 	  attr_accessor :check_outdated_callbacks
-
+	  attr_accessor :add_box_callbacks
+	  attr_accessor :prepare_clone_callbacks
+	  
 	  def initialize
 		@check_outdated_callbacks = []
+		@add_box_callbacks = []
+		@prepare_clone_callbacks = []
 	  end
 	  
-	  def check_outdated(&proc)
+	  def on_check_outdated(&proc)
 		@check_outdated_callbacks << {:mproc => proc}
+	  end
+	  
+	  def on_add_box(&proc)
+		@add_box_callbacks << {:mproc => proc}
+	  end
+	  
+	  def on_prepare_clone(&proc)
+		@prepare_clone_callbacks << {:mproc => proc}
 	  end
 	end
 	
@@ -19,12 +31,10 @@ module VagrantPlugins
         end
 		
         def call(env)
-		  @host = env[:host]
-		  
 		  callbacks = @callbacks_list.call
 		  
 		  callbacks.each do | clbk |
-			clbk[:mproc].call(@host)
+			clbk[:mproc].call(env)
 		  end
 		 
 		  @app.call(env)
@@ -36,7 +46,19 @@ module VagrantPlugins
 			super(app, env, Proc.new { env[:machine].config.vagrant_hook.check_outdated_callbacks } )
         end
 	end
+	
+	class AddBoxHook < HookActionAbstract
+		def initialize(app, env)
+			super(app, env, Proc.new { env[:machine].config.vagrant_hook.add_box_callbacks } )
+        end
+	end
 
+	class PrepareCloneHook < HookActionAbstract
+		def initialize(app, env)
+			super(app, env, Proc.new { env[:machine].config.vagrant_hook.prepare_clone_callbacks } )
+        end
+	end
+	
     class Plugin < Vagrant.plugin("2")
 		name "vagrant_hook"
 		
@@ -46,6 +68,14 @@ module VagrantPlugins
 		
 		action_hook(:vagrant_hook, Plugin::ALL_ACTIONS) do |hook|
 			hook.after(Vagrant::Action::Builtin::BoxCheckOutdated, VagrantPlugins::VagrantHook::HookBoxCheckOutdated)
+		end
+		
+		action_hook(:vagrant_hook, Plugin::ALL_ACTIONS) do |hook|
+			hook.after(Vagrant::Action::Builtin::BoxAdd, VagrantPlugins::VagrantHook::AddBoxHook)
+		end
+		
+		action_hook(:vagrant_hook, Plugin::ALL_ACTIONS) do |hook|
+			hook.after(Vagrant::Action::Builtin::PrepareClone, VagrantPlugins::VagrantHook::PrepareCloneHook)
 		end
 	end
   end
--- lib/vagrant_hook/version.rb
+++ lib/vagrant_hook/version.rb
@@ -1,5 +1,5 @@
 module VagrantPlugins
   module VagrantHook
-    VERSION = "0.1.2"
+    VERSION = "0.1.5"
   end
 end
